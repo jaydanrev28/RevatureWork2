fde59e55d86bbbe285bb4867c1c57b859b733e98
jaydanrev28 <jaydan.jacobs@revature.net>
2021-07-28 17:54:24 -0400

    Day 3

---
+++ Practice3/src/Practice1.java
@@ -0,0 +1,13 @@
+public class Practice1 {
+    public static void main(String[] args) {
+        String s1 = "Purple";
+        char ch[] = {'P','u','r','p','l','e'};
+
+        String s2 = new String(ch);
+        String s3 = new String("Rain");
+
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+    }
+}
