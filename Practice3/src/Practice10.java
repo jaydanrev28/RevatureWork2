96299751c1b34fe880145108238254bdcfc7bc68
jaydanrev28 <jaydan.jacobs@revature.net>
2021-07-28 23:17:08 -0400

    Day 3 example

--- Practice3/src/Practice10.java
+++ Practice3/src/Practice10.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 public class Practice10 {
     public static void main(String[] args) {
         String message = "Hello, Welcome to the Purple Rain listening party!!!";
-        String[] result = message.split("//");
+        String[] result = message.split("\\,");
         System.out.println(Arrays.toString(result));
     }
 }
